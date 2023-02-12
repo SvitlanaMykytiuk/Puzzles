06256b449613d3bf6fad3277aabb59d7875fd65b
SvitlanaMykytiuk <svetlana.mikityuk@gmail.com>
2023-02-12 12:50:02 +0200

    hw (from 06.02)

--- src/Main.java
+++ src/Main.java
@@ -8,28 +8,20 @@ public class Main {
         int secondStep = firstStep * 2;
         int thirdStep = letterPuzzle.length();
 
-        System.out.println(firstStep);
-        System.out.println(secondStep);
-        System.out.println(thirdStep);
-
-
         Stack<Character> firstPart = new Stack<>();
         for (int i = 0; i < firstStep; i++) {
             firstPart.add(letterPuzzle.charAt(i));
         }
-        System.out.println(firstPart);
 
         Stack<Character> secondPart = new Stack<>();
         for (int i = firstStep; i < secondStep; i++) {
             secondPart.add(letterPuzzle.charAt(i));
         }
-        System.out.println(secondPart);
 
         Stack<Character> thirdPart = new Stack<>();
         for (int i = secondStep; i < thirdStep; i++) {
             thirdPart.add(letterPuzzle.charAt(i));
         }
-        System.out.println(thirdPart);
 
         for (int i = 0; i < firstStep; i++) {
             System.out.print(firstPart.pop());
