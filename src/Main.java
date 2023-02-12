0e67ca21f29a8a4f2dfb40c1547ff9713bacdfa3
SvitlanaMykytiuk <svetlana.mikityuk@gmail.com>
2023-02-12 12:48:52 +0200

    hw (from 06.02)

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,43 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
 
-        Stack<String> firstPart = new Stack<>();
-        firstPart.add("е");
-        firstPart.add("в");
-        firstPart.add("и");
-        firstPart.add("р");
-        firstPart.add("П");
+        String letterPuzzle = "евирПим ,тр";
+        int firstStep = 5;
+        int secondStep = firstStep * 2;
+        int thirdStep = letterPuzzle.length();
 
-        Stack<String> secondPart = new Stack<>();
-        secondPart.add("и");
-        secondPart.add("м");
-        secondPart.add(" ");
-        secondPart.add(",");
-        secondPart.add("т");
+        System.out.println(firstStep);
+        System.out.println(secondStep);
+        System.out.println(thirdStep);
 
-        Stack<String> thirdPart = new Stack<>();
-        thirdPart.add("р");
 
-        System.out.print(firstPart.pop() + firstPart.pop() + firstPart.pop() + firstPart.pop() + firstPart.pop());
-        System.out.print(secondPart.pop() + secondPart.pop() + secondPart.pop() + secondPart.pop() + secondPart.pop());
-        System.out.print(thirdPart.pop());
+        Stack<Character> firstPart = new Stack<>();
+        for (int i = 0; i < firstStep; i++) {
+            firstPart.add(letterPuzzle.charAt(i));
+        }
+        System.out.println(firstPart);
+
+        Stack<Character> secondPart = new Stack<>();
+        for (int i = firstStep; i < secondStep; i++) {
+            secondPart.add(letterPuzzle.charAt(i));
+        }
+        System.out.println(secondPart);
+
+        Stack<Character> thirdPart = new Stack<>();
+        for (int i = secondStep; i < thirdStep; i++) {
+            thirdPart.add(letterPuzzle.charAt(i));
+        }
+        System.out.println(thirdPart);
+
+        for (int i = 0; i < firstStep; i++) {
+            System.out.print(firstPart.pop());
+        }
+        for (int i = firstStep; i < secondStep; i++) {
+            System.out.print(secondPart.pop());
+        }
+        for (int i = secondStep; i < thirdStep; i++) {
+            System.out.print(thirdPart.pop());
+        }
+
     }
 }
